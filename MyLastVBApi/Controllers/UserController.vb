e4a298673d3091290a95e0d0760e256f64df4314
‘Yonatan <yonatan@yaltman.com>
2020-03-01 17:30:24 +0200

    add api project

---
+++ MyLastVBApi/Controllers/UserController.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    Public Class UserController
+        Inherits ApiController
+
+        ' GET: api/User
+        Public Function GetValues() As IEnumerable(Of String)
+
+            Dim arr As List(Of String) = New List(Of String)() From {
+                "Yonatan",
+                "Maayan",
+                "Harry",
+                "Orya",
+                "Uri"
+            }
+
+
+            Return arr
+        End Function
+
+        ' GET: api/User/5
+        Public Function GetValue(ByVal id As Integer) As String
+            Return "value"
+        End Function
+
+        ' POST: api/User
+        Public Sub PostValue(<FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' PUT: api/User/5
+        Public Sub PutValue(ByVal id As Integer, <FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' DELETE: api/User/5
+        Public Sub DeleteValue(ByVal id As Integer)
+
+        End Sub
+    End Class
+End Namespace
---
+++ MyLastVBApi/Views/Home/Index.vbhtml
@@ -0,0 +1,20 @@
+﻿<div class="jumbotron">
+    <h1>Api איזה כיף</h1>
+    
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <div>
+
+            <button class="btn">Search</button>
+
+
+
+
+        </div>
+    </div>
+    <div class="col-md-6">
+        <button class="btn">Update</button>
+    </div>
+    
+</div>
